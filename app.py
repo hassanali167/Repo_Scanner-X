8a4bf8898087ac52dc4396adfd2aa3287dbc1f05
AarkayITsolutions <alihassanali119683@gmail.com>
2025-04-15 14:24:07 +0500

    updating UI-Adding Downloading Report FUnction-Commands Execution addition

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ SCAN_HISTORY = {}
 PROJECT_TITLE = "Repo Scanner-X"
 HEADING = "# Repo Scanner-X"
 HEADING_ALT = "# 🛡️ Git Vulnerability Scanner and AI-based Recommendation System"
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "YOur Groq Api"
 GROQ_ENDPOINT = "https://api.groq.com/openai/v1/chat/completions"
 GROQ_MODEL = "llama3-70b-8192"
 
